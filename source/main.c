d75ceb820213b277e711f8081c5e65100e87a9b1
Astraea Shao <ashao001@wch136-17.cs.ucr.edu>
2020-04-11 14:31:38 -0700

    fixing p1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: ashao001
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #3  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -18,26 +18,20 @@ int main(void) {
 	DDRB = 0x00; PORTB = 0XFF; //input
 	DDRC = 0XFF; PORTC = 0X00; //OUTPUT
     /* Insert your solution below */
-    unsigned char countA, lvlcnt, countB, tempA;
+    unsigned char countA, tempA;
     countA = 0x00;
-    countB = 0x00;
-    lvlcnt = 0;
     while (1) {
 	if((PINA & 0x01) == 0x01){
 		countA++;
-		lvlcnt++;
 	}	
 	if((PINA & 0x02) == 0x02){
 		countA++;
-		lvlcnt++;
 	}
 	if((PINA & 0x04) == 0x04){
 		countA++;
-		lvlcnt++;
 	}
 	if((PINA & 0x08) == 0x08){
 		countA++;
-		lvlcnt++;
 	}
 	if((PINA & 0x10) == 0x10){
 		countA++;
@@ -53,54 +47,32 @@ int main(void) {
 	}	
 	//PINB
 	if((PINB & 0x01) == 0x01){
-                countB++;
+                countA++;
 }
 	 if((PINB & 0x02) == 0x02){
-countB++;
+countA++;
 }
         if((PINB & 0x04) == 0x04){
-countB++; 
+countA++; 
 }  
 if((PINB & 0x08) == 0x08){
-                countB++;
+                countA++;
 }
         if((PINB & 0x10) == 0x10){
-                countB++;
+                countA++;
 }
         if((PINB & 0x20) == 0x20){
-                countB++;
+                countA++;
         }
         if((PINB & 0x40) == 0x40){
-                countB++;
+                countA++;
         }
 	if((PINB & 0x80) == 0x80){
-		countB++;
+		countA++;
 	}
 	
-	//for exercise 2
-	if((((PINA & 0x01)==0x01) || ((PINA & 0x02) == 0x02))&& (lvlcnt == 1 )){
-	PORTC = 0X60; //1 or 2
-	}
-	if( (((PINA & 0x03)==0x03) && (lvlcnt == 2)) || (((PINA & 0x04)==0x04) && (lvlcnt == 1)) ){
-	PORTC = 0x70; // 3 or 4
-	}
-	if( (((PINA & 0x05)==0x05) && (lvlcnt == 2)) || (((PINA & 0x06)==0x06) && (lvlcnt == 2)) ){
-	PORTC = 0x38; //5 or 6
-	}
-	if( (((PINA & 0x07)==0x07) && (lvlcnt == 3)) || (((PINA & 0x08)==0x08) && (lvlcnt == 1)) || (((PINA & 0x09)==0x09) && (lvlcnt == 2)) ){
-	PORTC = 0X3C; // 7 or 8 or 9
-	}
-	if( (((PINA & 0x0A)==0x0A) && (lvlcnt == 2)) || (((PINA & 0x0B)==0x0B) && (lvlcnt == 3)) || (((PINA & 0x0C)==0x0C) && (lvlcnt == 2)) ){
-	PORTC = 0X3E; // 10 or 11 or 12
-	}
-	if( (((PINA & 0x0D)==0x0D) && (lvlcnt == 3)) || (((PINA & 0x0E)==0x0E) && (lvlcnt == 3)) || (((PINA & 0x0F)==0x0F) && (lvlcnt == 4)) ){
-	PORTC = 0x3F;
-	}
-
-	//PORTC = countA + countB;
+	PORTC = countA;
 	countA = 0x00;
-	countB = 0x00;
-	lvlcnt = 0;
     }
     return 1;
 }
